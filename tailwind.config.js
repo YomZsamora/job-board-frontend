6faa9cd86f9cbc7b0c28e036662fed7fdfd5b9f4
yomZsamora <samaurah@gmail.com>
2022-05-10 13:44:51 +0300

    Install tailwindcss and its peer dependencies via npm.

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    colors: {
+      default: '#0F172A',
+      white: '#ffffff',
+      'btn-dark': '#14283F',
+      'btn-light': '#193952',
+      'blue-light': '#41BEF6',
+      'input-default': '#1E293B',
+      'grey': {
+        700: '#91A0B3'
+      }
+    }
+  },
+  plugins: [],
+}
