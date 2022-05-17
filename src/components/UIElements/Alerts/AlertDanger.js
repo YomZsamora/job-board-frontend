750efe7684bc4a79a4ed9b1b9d8cf2cce041dcb7
yomZsamora <samaurah@gmail.com>
2022-05-17 22:26:57 +0300

    Done: Redesign of Alerts.

--- src/components/UIElements/Alerts/AlertDanger.js
+++ src/components/UIElements/Alerts/AlertDanger.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-function AlertDanger({title, message}) {
+function AlertDanger({message}) {
     return (
         <div className="bg-alert-danger-light border-t-4 border-alert-danger-dark rounded-b text-alert-danger-dark px-4 py-3 shadow-md my-2" role="alert">
             <div className="flex">
-            <FontAwesomeIcon className="h-6 w-6 mr-4" icon="fa-solid fa-ban" />
+            <FontAwesomeIcon className="h-4 w-4 mr-4" icon="fa-solid fa-ban" />
                 <div>
-                <p className="text-nunito-semiBold text-sm">{title}</p>
                 <p className="text-xs">{message}</p>
                 </div>
             </div>
         </div>
+
+        
     )
 }
 
