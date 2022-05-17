61c28e7aed75689ef8b693eaeb6e5668e7e9d5b0
yomZsamora <samaurah@gmail.com>
2022-05-17 17:37:34 +0300

    Setting up student bulk upload.

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -56,12 +56,12 @@ function Navbar({user, logout, showBulkUpload}) {
                 <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto">
                     <button type="button" className="p-1 hover:text-secondary">
                         <span className="sr-only">Add Employer</span>
-                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.2" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"></path></svg>
+                        <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.2" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"></path></svg>
                     </button>
 
                     <button onClick={bulkUpload} type="button" className="ml-2 p-1 hover:text-secondary focus:text-secondary">
                         <span className="sr-only">Bulk Upload</span>
-                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.2" d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path></svg>
+                        <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.2" d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path></svg>
                     </button>
                     
                     <div className="ml-3 relative">
--- src/components/outcomes/cohorts/Cohorts.js
+++ src/components/outcomes/cohorts/Cohorts.js
@@ -15,37 +15,22 @@ function Cohorts() {
                     
                     <div class="relative bg-white rounded-lg shadow dark:bg-gray-700 p-6">
                         
-                        <div class="flex justify-between items-center">
+                        <div class="flex justify-between items-center mb-6">
                             <h3 className="text-xl leading-6 text-nunito-bold text-secondary" id="modal-title">Add New Employer</h3>
                             <button type="button" class="text-primary hover:text-alert-danger-dark rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" >
                                 {/* <FontAwesomeIcon icon="fa-solid fa-xmark" /> */}
                             </button>
                         </div>
                         
-                        <div className="text-center sm:mt-0 sm:text-left">
-                        {/* <h3 className="text-xl leading-6 text-nunito-bold text-secondary" id="modal-title">Bulk Upload Students</h3> */}
-                        <div className="mt-2 text-primary">
-                            <h3 className="text-md leading-6 text-nunito-bold" id="modal-title">How to Upload</h3>
-                            <ol className="list-decimal pl-4 mt-2 text-xs">
-                                <li>Download a <a className="text-secondary" href="/">template here</a>.</li>
-                                <li>Add your data to the template. <br></br> <span className="italic">Using excel make sure to export or save as .csv</span></li>
-                                <li>Upload below for processing.</li>
-                            </ol>
-                            <div className="mt-4 flex justify-center px-6 pt-5 pb-6 border-2 border-primary-light border-dashed bg-gray w-full">
-                                <div className="space-y-1 text-center">
-                                    <div className="flex text-xs text-primary mb-8">
-                                            <label
-                                                htmlFor="file-upload"
-                                                className="relative cursor-pointer rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-secondary">
-                                                <span className="text-secondary">Upload filled in template here.</span>
-                                                <input id="file-upload" name="file-upload" type="file" className="sr-only" accept=".csv" />
-                                            </label>
-                                            <p className="pl-2">csv format only.</p>
-                                    </div>
-                                    <p className="text-[10px]"></p>
-                                </div>
-                            </div>
-                        </div>
+                        <div className="mb-4">
+                            <label className="block text-sm mb-1">Company Name</label>
+                            <input 
+                                className="w-96 text-xs text-primary ring-2 ring-primary-light py-2 px-2 text-nunito-light focus:outline-none focus:ring-2 focus:ring-secondary" 
+                                type="text" 
+                                autoComplete="off" 
+                                placeholder="Enter company's name" 
+                                id="password"  
+                                name="password" />
                         </div>
                     </div>
                 </div>
